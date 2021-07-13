586f87ed6cbcdf839a254c2f4a33e45e14f44a1e
Anu48 <anoushka212@gmail.com>
2021-07-13 13:34:46 -0400

    Changed the voice of the AI

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,12 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init()
-engine.say("hello how are you?")
-engine.runAndWait()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id) # 0 -> male voice; 1 -> female voice
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+speak("Hello")
